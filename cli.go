b37d5852a9c0f9c13693b99a6bd9a39423a933b2
vx-clutch <owestness@gmail.com>
2024-09-16 20:15:29 -0400

    Format string support

--- cli.go
+++ cli.go
@@ -15,34 +15,56 @@ const (
 	errC   = "\033[1;91m" // Bold High Intensity Red
 	warnC  = "\033[1;95m" // Bold High Intensity Purple
 	debugC = "\033[1;97m" // Bold High Intensity White
-	mname  = ""
 )
 
+var (
+	mname = "call Setup()"
+)
+
+// Gives project name to program, MUST be called first
 func Setup(name string) {
 	mname = name
 }
 
-func Fatal(m any) {
+// Fatal call exits with zero, takes a fatal error message
+func Fatal(m string) {
 	fmt.Printf("%s: %sfatal error%s: %s", mname, errC, reset, m)
 	fmt.Println("\ncompilation terminated.")
 	os.Exit(0)
 }
 
-func Fatalf(m any, args ...string) string {
-	fmt.Printf("%s: %serror%s: %s\n", mname, errC, reset, fmt.Sprintf(m, ...args))
-	return fmt.Sprintf("%s: %serror%s: %s\n", mname, errC, reset, m)
+// Fatal call exits with zero, Takes a error message and a formatted string
+func Fatalf(m string, args ...interface{}) {
+	fmt.Printf("%s: %serror%s: %s\n", mname, errC, reset, fmt.Sprintf(m, args...))
+	fmt.Println("compilation terminated.")
+	os.Exit(0)
 }
 
-func Error(m any) string {
+// Error call does not exit, Takes a error message
+func Error(m string) string {
 	fmt.Printf("%s: %serror%s: %s\n", mname, errC, reset, m)
 	return fmt.Sprintf("%s: %serror%s: %s\n", mname, errC, reset, m)
 }
 
-func Warn(warning any) string {
+// Error call does not exit, Takes a error message and a formatted string
+func Errorf(m string, args ...interface{}) string {
+	fmt.Printf("%s: %serror%s: %s\n", mname, errC, reset, fmt.Sprintf(m, args...))
+	return fmt.Sprintf("%s: %serror%s: %s\n", mname, errC, reset, fmt.Sprintf(m, args...))
+}
+
+// Warn call does not exit, Takes a warning message
+func Warn(warning string) string {
 	fmt.Printf("%swarning:%s %s\n", warnC, reset, warning)
 	return fmt.Sprintf("%s: %serror%s: %s\n", mname, errC, reset, warning)
 }
 
+// Warn call does not exit, Takes a warning message and a formatted string
+func Warnf(warning string, args ...interface{}) string {
+	fmt.Printf("%swarning:%s %s\n", warnC, reset, fmt.Sprintf(warning, args...))
+	return fmt.Sprintf("%s: %serror%s: %s\n", mname, errC, reset, warning)
+}
+
+// Debug call does not exit. can be passed a tag and a debug message
 func Debug(tag string, dbm any) string {
 	fmt.Printf("%sdebug%s: %s\n%s\n", debugC, reset, tag, dbm)
 	return fmt.Sprintf("%sdebug%s: %s\n%s\n", debugC, reset, tag, dbm)
